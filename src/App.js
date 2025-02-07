d14f9b3ce67402d33013476761bee07ba24666b6
dougiehawes <dougiehawes@hotmail.com>
2025-02-07 16:42:27 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import axios from "axios";
+
+import { useState, useEffect } from "react";
+
 import "./style.scss";
 
 const App = () => {
-  return <div>App</div>;
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const response = await axios.get("http://localhost:5000/home");
+
+      setMessage(response.data.msg);
+    };
+
+    fetchMessage();
+  }, []);
+
+  return <div>{message}</div>;
 };
 
 export default App;
